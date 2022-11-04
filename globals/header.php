99e9958d44db0bcf3b1284de99bd759cb17c3406
Michael McKay <mckaymichaelk@gmail.com>
2022-11-04 12:14:30 -0700

    home page and global styles started

--- globals/header.php
+++ globals/header.php
@@ -1,10 +1,16 @@
 <header>
-    <div class="logo-header">
+    <div class="logo-header logo">
         <a href="index.php">
             <?php require('images/logo.svg');?>
         </a>
     </div>
     <nav>
-        
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Skills</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
     </nav>
 </header>
--- index.php
+++ index.php
@@ -2,6 +2,27 @@
 <?php require_once('globals/head.php');?>
 <body>
     <?php require('globals/header.php');?>
+    <div class="theme-wrapper">
+        <main>
+            <section id="sec1">
+                <div>
+                    <div class="logo">
+                        <?php require('images/logo.svg');?>
+                    </div>
+                    <div class="titles">
+                        <h1>Web<br>Developer</h1>
+                        <h2>Michael McKay</h2>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <h2>Featured Projects</h2>
+                <h3>First Website</h3>
+                <h3>Vizro Records</h3>
+            </section>
+    
+        </main>
+    </div>
     <?php require('globals/footer.php');?>
 </body>
 </html>
