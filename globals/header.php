4e404f1cf22d46be140a8d00ed575417217ad9dc
mckaymichael <mckaymichaelk@gmail.com>
2022-11-12 13:10:49 -0800

    adding links

--- globals/header.php
+++ globals/header.php
@@ -1,11 +1,13 @@
 <header>
 	<div class="logo-container">
-		<?php require('images/logo.svg')?>
+		<a href="index.php">
+			<?php require('images/logo.svg')?>
+		</a>
 	</div>
 	<nav class="header-nav">
 		<ul>
 			<li><a href="index.php"><i class="fa-solid fa-house" title="Home"></i><span>Home</span></a></li>
-			<li><a href="#"><i class="fa-solid fa-user xl" title="About me"></i><span>About</span></a></li>
+			<li><a href="about.php"><i class="fa-solid fa-user xl" title="About me"></i><span>About</span></a></li>
 			<li><a href="#"><i class="fa-solid fa-phone lg" title="Contact"></i><span>Contact</span></a></li>
 			<li><a href="#"><i class="fa-solid fa-screwdriver-wrench" title="Skills"></i><span>Skills</span></a></li>
 			<li><a href="#"><i class="fa-solid fa-pen-ruler" title="Projects"></i><span>Projects</span></a></li>
