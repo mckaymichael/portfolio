279102af025880b409af5cac2b808fd48b8f5288
mckaymichael <mckaymichaelk@gmail.com>
2022-11-11 21:07:16 -0800

    finished mobile version of website

--- globals/header.php
+++ globals/header.php
@@ -1,14 +1,14 @@
 <header>
+	<div class="logo-container">
+		<?php require('images/logo.svg')?>
+	</div>
 	<nav class="header-nav">
 		<ul>
-			<li>Home</li>
-			<li>Contact</li>
-			<li>About Me</li>
-			<li>Skills</li>
-			<li>Projects</li>
+			<li><a href="index.php"><i class="fa-solid fa-house" title="Home"></i><span>Home</span></a></li>
+			<li><a href="#"><i class="fa-solid fa-user xl" title="About me"></i><span>About</span></a></li>
+			<li><a href="#"><i class="fa-solid fa-phone lg" title="Contact"></i><span>Contact</span></a></li>
+			<li><a href="#"><i class="fa-solid fa-screwdriver-wrench" title="Skills"></i><span>Skills</span></a></li>
+			<li><a href="#"><i class="fa-solid fa-pen-ruler" title="Projects"></i><span>Projects</span></a></li>
 		</ul>
-		<div class="menu-btn" onclick="openClose()">
-			<i class="arrow fa-solid fa-arrow-down"></i>
-		</div>
 	</nav>
 </header>
--- index.php
+++ index.php
@@ -5,22 +5,49 @@
     <div class="theme-wrapper">
         <main>
             <section id="banner">
-                <div>
-                    <div>
-                        <?php require('images/logo.svg');?>
-                    </div>
-                    <div class="titles">
-                        <h1>Web<br>Developer</h1>
-                        <h2>Michael McKay</h2>
-                    </div>
+                <div class="logo-container">
+                    <?php require('images/logo.svg');?>
+                </div>
+                <div class="titles">
+                    <h2>Michael McKay</h2>
+                    <h1 class="nickainley-font">Full Stack Web Developer <br>&amp;<br> UX Developer</h1>
                 </div>
             </section>
             <section id="featured">
+                <h2 class="nickainley-font">Featured Projects</h2>
+                <div class="feature-blocks">
+                    <div class="content">
+                        <h3>JavaScript</h3>
+                        <div class="image-container">
+                            <img src="images/plant.jpg" alt="">
+                        </div>
+                        <div class="chip-container">
+                            <div class="chip">
+                                <h4>Solo</h4>
+                            </div>
+                            <div class="chip">
+                                <h4>Coding</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <h3>Vizro Records</h3>
+                        <div class="image-container">
+                            <img src="images/plant.jpg" alt="">
+                        </div> 
+                        <div class="chip-container">
+                            <div class="chip">
+                                <h4>Group Project</h4>
+                            </div> 
+                            <div class="chip">
+                                <h4>Wireframing</h4>
+                            </div>               
+                        </div>
+                    </div>
+                </div>
             </section>
         </main>
     </div>
     <?php require('globals/footer.php');?>
 </body>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<?php include('script/main.js'); ?>
 </html>
