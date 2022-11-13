6349617f211423c85eaf4afffcaca06dff7c8c84
mckaymichael <mckaymichaelk@gmail.com>
2022-11-12 16:28:58 -0800

    finished about me page

---
+++ about.php
@@ -0,0 +1,36 @@
+<?php require_once('init.php')?>
+<?php require_once('globals/head.php');?>
+<body>
+    <?php require('globals/header.php');?>
+    <div class="theme-wrapper">
+        <main>
+            <section class="banner-sec">
+                <div class="images-of-me">
+                    <img class="portrait" src="images/me1.jpg" alt="front facing camera of man's face">
+                    <img class="portrait" src="images/me2.jpg" alt="front facing camera of man's face">
+                    <img class="portrait" src="images/me3.jpg" alt="front facing camera of man's face">
+                </div>
+                <div class="content">
+                    <h1 class="nickainley-font">About Me</h1>
+                    <p>My name is Michael McKay. It took me a while to find what I love, but ultimately that passion became coding and art. I love to learn, and it's usually what I spend my time doing outside of school. If it's not learning how to draw, code, linguistics, or design, I'm either climbing, coaching basketball, doing photography, or walking my dogs.</p>
+                </div>
+            </section>
+            <section class="core-values-sec">
+                <h2 class="nickainley-font">Core Values</h2>
+                <div class="values">
+                    <button>Respect</button>
+                    <button>Trust</button>
+                    <button>Openness</button>
+                    <button>Growth</button>
+                    <button>Enthusiasm</button>
+                </div>
+            </section>
+            <section class="mission-sec">
+                <h2 class="nickainley-font">Mission</h2>
+                <p>I value growth above all else. Becoming the best version of myself allows me to bring the best out of the people around me. My mission is to never stop trying to grow and better myself.</p>
+            </section>
+        </main>
+    </div>
+    <?php require('globals/footer.php');?>
+</body>
+</html>
