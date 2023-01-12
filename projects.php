cb1222bb51930a58519a1e455f2c8306a241fb8a
Michael McKay <mckaymichaelk@gmail.com>
2023-01-12 14:57:30 -0800

    removed 3 projects
    JS website, BirghtSky Wireframe Projects

--- projects.php
+++ projects.php
@@ -4,36 +4,12 @@
     <?php require('globals/header.php');?>
     <div class="theme-wrapper">
         <main class="projects">
-            <section class="brightsky-mob flex">
-                <div class="proj-img-display"></div>
-                <div class="content">
-                    <h1 class="nickainley-font">BrightSky Mobile</h1>
-                    <p class="text">BrightSky is a personal project of mine. It&apos;s an encyclopedia page for stars and constellations. The goal for it was to make a website that was interactive so that kids who are curious can genuinely enjoy exploring the website.</p>
-                    <button><a href="brightsky-mobile.php"> See More</a></button>
-                </div>
-            </section>
             <section class="vizro flex flex-reverse">
                 <div class="proj-img-display"></div>
                 <div class="content">
                     <h2 class="nickainley-font">Vizro</h2>
                     <p class="text">Vizro was a group project and is a made up record label company that&apos;s purpose was to bring to light unknown, yet talented artists. Its aim is to be a mobile application.</p>
                     <button> <a href="vizro.php">See More</a></button>
-                </div>
-            </section>
-            <section class="js-web flex">
-                <div class="proj-img-display"></div>
-                <div class="content">
-                    <h2 class="nickainley-font">JS Website</h2>
-                    <p class="text">This is the largest coding project I&apos;ve done. It goes over javascript definitions with examples and demonstrates my ability to use animations well and a strong understanding of CSS and JavaScript.</p>
-                    <button> <a href="js-web.php">See More</a></button>
-                </div>
-            </section>
-            <section class="brightsky-web flex flex-reverse">
-                <div class="proj-img-display"></div>
-                <div class="content padding-bottom">
-                    <h2 class="nickainley-font">BrightSky Website</h2>
-                    <p class="text">BrightSky is a personal project of mine. This is the desktop version. It&apos;s an encyclopedia page for starts and constellations. The goal for it was to make a website that was interactive so that kids who are curious can genuinely enjoy exploring the website.</p>
-                    <button><a href="brightsky-web.php"> See More</a></button>
                 </div>
             </section>
         </main>
